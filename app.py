ad556fe9a0c1ac60db3a6dea49fce48d79e09c05
SubodhSingh8543 <subodhsingh8543@gmail.com>
2023-07-24 17:22:09 +0530

    done24

--- app.py
+++ app.py
@@ -498,4 +498,4 @@ def login():
 
 if __name__ == '__main__':
     # print("lsadjablasdbhjkhds",mongo_uri,dotenv_path)
-    app.run(port=11000)
+    app.run(port=10000)
--- run.py
+++
@@ -1,2 +0,0 @@
-from myapp import appname
-app = appname()
